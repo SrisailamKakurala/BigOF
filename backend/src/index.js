d9764513488bd43079d1eeb7f1a066d5115592e4
srisailamkakurala <srisailamkakurala@gmail.com>
2024-09-02 20:35:41 +0530

    re-push

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,30 +16,3 @@ connectDB()
 .catch((err) => {
     console.log('Mongodb connection failed: ' + err.message)
 })
-
-
-
-
-
-
-
-
-
-/*
-( async () => {
-    try{
-        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
-        app.on('error', (err) => {
-            console.log(err)
-            throw err
-        })
-
-        app.listen(process.env.PORT, () => {
-            console.log('server listening on port ' + process.env.PORT)
-        })
-    }catch(err){
-        console.log(err)
-        throw err
-    }
-})()
-*/
